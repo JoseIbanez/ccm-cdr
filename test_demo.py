ac3001dac2fdb1bd9f5dc7447e458766ef3ca7cd
José Ibañez <ibanez.j@gmail.com>
2016-12-08 18:12:37 +0100

    Initial version

---
+++ test_demo.py
@@ -0,0 +1,28 @@
+import unittest
+import cdr
+
+class TestCdrMethods(unittest.TestCase):
+
+    @unittest.skip("demo version")
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_time_to_string(self):
+        self.assertEqual(cdr.time_to_string("1447409455"), '2015-11-13 11:10:55')
+
+    def test_int_to_ip(self):
+        self.assertEqual(cdr.int_to_ip("51388076"), '172.30.16.3')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper(),"oooh :(")
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
