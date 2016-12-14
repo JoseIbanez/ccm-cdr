436798ee14285d321893f0f61f30094920034430
José Ibañez <ibanez.j@gmail.com>
2016-12-14 18:38:09 +0100

    test

--- cmr.py
+++ cmr.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-# cat ./cmr_StandAloneCluster_01_20150421* | awk -F "\"*,\"*" '{print $3,$5,$7,$8,$10,$12,$13,$14,$18,$19}' | sort 
+# cat ./cmr_StandAloneCluster_01_20150421* | awk -F "\"*,\"*" '{print $3,$5,$7,$8,$10,$12,$13,$14,$18,$19}' | sort
 # "cdrRecordType","globalCallID_callManagerId","globalCallID_callId","nodeId","directoryNum","callIdentifier","dateTimeStamp","numberPacketsSent","numberOctetsSent","numberPacketsReceived","numberOctetsReceived","numberPacketsLost","jitter","latency","pkid","directoryNumPartition","globalCallId_ClusterID","deviceName","varVQMetrics"
-# 0               1                             2                     3        4              5                6               7                   8                  9                       10                     11                  12       13        14     15                      16                      17                                                      
+# 0               1                             2                     3        4              5                6               7                   8                  9                       10                     11                  12       13        14     15                      16                      17
 
 
 #"cdrRecordType","globalCallID_callManagerId","globalCallID_callId","origLegCallIdentifier","dateTimeOrigination","origNodeId","origSpan","origIpAddr","callingPartyNumber","callingPartyUnicodeLoginUserID","origCause_location","origCause_value","origPrecedenceLevel","origMediaTransportAddress_IP","origMediaTransportAddress_Port","origMediaCap_payloadCapability","origMediaCap_maxFramesPerPacket","origMediaCap_g723BitRate","origVideoCap_Codec","origVideoCap_Bandwidth","origVideoCap_Resolution","origVideoTransportAddress_IP","origVideoTransportAddress_Port","origRSVPAudioStat","origRSVPVideoStat","destLegIdentifier","destNodeId","destSpan","destIpAddr","originalCalledPartyNumber","finalCalledPartyNumber","finalCalledPartyUnicodeLoginUserID","destCause_location","destCause_value","destPrecedenceLevel","destMediaTransportAddress_IP","destMediaTransportAddress_Port","destMediaCap_payloadCapability","destMediaCap_maxFramesPerPacket","destMediaCap_g723BitRate","destVideoCap_Codec","destVideoCap_Bandwidth","destVideoCap_Resolution","destVideoTransportAddress_IP","destVideoTransportAddress_Port","destRSVPAudioStat","destRSVPVideoStat","dateTimeConnect","dateTimeDisconnect","lastRedirectDn","pkid","originalCalledPartyNumberPartition","callingPartyNumberPartition","finalCalledPartyNumberPartition","lastRedirectDnPartition","duration","origDeviceName","destDeviceName","origCallTerminationOnBehalfOf","destCallTerminationOnBehalfOf","origCalledPartyRedirectOnBehalfOf","lastRedirectRedirectOnBehalfOf","origCalledPartyRedirectReason","lastRedirectRedirectReason","destConversationId","globalCallId_ClusterID","joinOnBehalfOf","comment","authCodeDescription","authorizationLevel","clientMatterCode","origDTMFMethod","destDTMFMethod","callSecuredStatus","origConversationId","origMediaCap_Bandwidth","destMediaCap_Bandwidth","authorizationCodeValue","outpulsedCallingPartyNumber","outpulsedCalledPartyNumber","origIpv4v6Addr","destIpv4v6Addr","origVideoCap_Codec_Channel2","origVideoCap_Bandwidth_Channel2","origVideoCap_Resolution_Channel2","origVideoTransportAddress_IP_Channel2","origVideoTransportAddress_Port_Channel2","origVideoChannel_Role_Channel2","destVideoCap_Codec_Channel2","destVideoCap_Bandwidth_Channel2","destVideoCap_Resolution_Channel2","destVideoTransportAddress_IP_Channel2","destVideoTransportAddress_Port_Channel2","destVideoChannel_Role_Channel2","IncomingProtocolID","IncomingProtocolCallRef","OutgoingProtocolID","OutgoingProtocolCallRef","currentRoutingReason","origRoutingReason","lastRedirectingRoutingReason","huntPilotPartition","huntPilotDN","calledPartyPatternUsage","IncomingICID","IncomingOrigIOI","IncomingTermIOI","OutgoingICID","OutgoingOrigIOI","OutgoingTermIOI","outpulsedOriginalCalledPartyNumber","outpulsedLastRedirectingNumber","wasCallQueued","totalWaitTimeInQueue","callingPartyNumber_uri","originalCalledPartyNumber_uri","finalCalledPartyNumber_uri","lastRedirectDn_uri","mobileCallingPartyNumber","finalMobileCalledPartyNumber","origMobileDeviceName","destMobileDeviceName","origMobileCallDuration","destMobileCallDuration","mobileCallType","originalCalledPartyPattern","finalCalledPartyPattern","lastRedirectingPartyPattern","huntPilotPattern"
-# 0               1                            2                     3                       4                     5            6          7            8                                          
+# 0               1                            2                     3                       4                     5            6          7            8
 
 import sys;
 import csv;
@@ -39,10 +39,10 @@ def int_to_ip(signed_int):
     # CUCM occasionally creates CDRs with an IP of '0'. Bug or feature? Beats me.
     if signed_int == 0:
         return "err_ip"
-    
-    # hex conversion for 32-bit signed int; 
+
+    # hex conversion for 32-bit signed int;
     # the slice at the end removes the '0x' and 'L' in the result
-    h = hex(signed_int & 0xffffffff)[2:-1] 
+    h = hex(signed_int & 0xffffffff)[2:-1]
 
     if len(h) == 7: #pad initial zero if required
         h = '0' + h
@@ -50,7 +50,7 @@ def int_to_ip(signed_int):
     hex_ip = [h[6:8],h[4:6],h[2:4],h[:2]] # reverse the octets
 
     #put them back together in IPv4 format
-    ip = '.'.join([str(int(n,16)) for n in hex_ip]) 
+    ip = '.'.join([str(int(n,16)) for n in hex_ip])
 
     return ip
 
@@ -62,16 +62,16 @@ reader = csv.reader(iter(sys.stdin.readline, ''))
 row = 1
 for row in reader:
 
-  row[6]=time_to_string(row[6])  
+  row[6]=time_to_string(row[6])
 
   #row[ 7]=int_to_ip(row[ 7])
 
-  # 3,5,7,8,10,12,13,14,18 
+  # 3,5,7,8,10,12,13,14,18
 
 
   qos=row[18].split(";")
   cs="0"
-  scs="0"	
+  scs="0"
   for parameter in qos:
 	try:
 		kpi,value = parameter.split("=",1)
@@ -83,6 +83,4 @@ for row in reader:
 		continue
 
 
-  print ','.join([row[6],row[4],row[7],row[9],row[11],row[12],row[13],cs,scs,row[16],row[2],row[17]])  
-  
-  
+  print(','.join([row[6],row[4],row[7],row[9],row[11],row[12],row[13],cs,scs,row[16],row[2],row[17]]))
